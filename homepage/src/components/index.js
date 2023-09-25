440b860715459d25e6fc97f9a054c2337ec27900
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2023-09-25 19:47:46 +0200

    remoe all

--- homepage/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Header } from "./header/Header";
-export { default as Navbar } from "./navbar/Navbar";
-export { default as Skill } from "./om-meg/Om";
-export { default as Download } from "./download/Download";
-export { default as Faq } from "./faq/Faq";
-export { default as Heading } from "./heading/Heading"
-export { default as Homepage } from "./homepage/Homepage"
